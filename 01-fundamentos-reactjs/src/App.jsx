7e2429a4fe9512a4fea9ff3e5092a4216dc7a4f5
Mateus Seixas <mateus.dev@hublocal.com.br>
2022-12-24 18:50:24 -0300

    Propriedades

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,10 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Mateus Seixas"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus itaque fugit vitae accusamus amet perspiciatis quibusdam! Tempore culpa quae, alias nisi totam adipisci debitis porro unde quos voluptatem, iusto maxime?"
+      />
+      <Post
+        author="Usuário Anonimo"
+        content="Novo post muito legal"
+      />
     </div>
   )
 }
--- 01-fundamentos-reactjs/src/Post.jsx
+++ 01-fundamentos-reactjs/src/Post.jsx
@@ -1,3 +1,8 @@
-export function Post() {
-  return <p>Post</p>
+export function Post(props) {
+  return (
+    <>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </>
+  )
 }
