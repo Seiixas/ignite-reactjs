954f8abb6a1972d620d1d78c9a84710c28584541
Mateus Seixas <mateus.dev@hublocal.com.br>
2022-12-24 19:00:11 -0300

    CSS Modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Mateus Seixas"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus itaque fugit vitae accusamus amet perspiciatis quibusdam! Tempore culpa quae, alias nisi totam adipisci debitis porro unde quos voluptatem, iusto maxime?"
