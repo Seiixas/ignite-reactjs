407940bcb500bb51089738970c3544c72a28d8d0
Mateus Seixas <mateus.dev@hublocal.com.br>
2023-01-03 12:42:58 -0300

    Formulario de comentarios

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -33,6 +33,17 @@ export function Post(props) {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentario"
+         />
+         <footer>
+          <button type="submit">Publicar</button>
+         </footer>
+      </form>
     </article>
   )
 }
