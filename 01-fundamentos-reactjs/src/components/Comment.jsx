3da2cc20486fcc97f39550038c331afdf58c7e29
Mateus Seixas <mateus.dev@hublocal.com.br>
2023-01-03 12:51:25 -0300

    Componente: Comment

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (<div className={styles.comment}>
+    <img src="https://github.com/Seiixas.png" alt="" />
+
+    <div className={styles.commentBox}>
+      <div className={styles.commentContent}>
+        <header>
+          <div className={styles.authorAndTime}>
+            <strong>Mateus Seixas</strong>
+            <time
+              title="11 de Maio as 08:13h"
+              dateTime="2022-05-11 08:13:24"
+            >
+              Cerca de 1h atras
+            </time>
+          </div>
+
+          <button title="Deletar comentario">
+            <Trash size={20} />
+          </button>
+        </header>
+        <p>Muito bom Devon, parabens!</p>
+      </div>
+      <footer>
+        <button><ThumbsUp /> Aplaudir<span>20</span></button>
+      </footer>      
+    </div>
+  </div>)
+}
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(props) {
@@ -44,6 +45,11 @@ export function Post(props) {
           <button type="submit">Publicar</button>
          </footer>
       </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
