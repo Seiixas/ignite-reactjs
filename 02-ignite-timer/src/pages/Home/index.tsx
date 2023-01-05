7186270cc26c555cf4b2b5ed57d6b94dc75660ad
Mateus Seixas <mateus.dev@hublocal.com.br>
2023-01-05 20:33:48 -0300

    [Projeto 02]: Aprimorando os inputs

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -18,14 +18,23 @@ export function Home() {
           <TaskInput
             type="text"
             id="task"
+            list="task-suggestions"
             placeholder="Digite um nome para o seu projeto"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            max={60}
+            min={5}
           />
 
           <span>minutos.</span>
