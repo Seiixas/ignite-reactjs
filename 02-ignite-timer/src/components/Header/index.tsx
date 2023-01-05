cca397dc6afd19bb7ac7df04b207bbf8235cd5c1
Mateus Seixas <mateus.dev@hublocal.com.br>
2023-01-05 19:59:59 -0300

    [Projeto 02]: Header & Layout

---
+++ 02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="Timer" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Historico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
